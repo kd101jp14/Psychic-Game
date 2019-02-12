8eca7fc5c1318d0c1bb59d5b900f2e0acdc79c51
ksd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-02-12 01:21:31 -0600

    Non-alphabet issue fixed. Game only responds to alphabet keys.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,48 +16,53 @@ window.onload = function () {
     console.log(chosenLet);
 
     // User presses a key to guess chosen letter. This key is captured for eval.
-    document.onkeyup = function(event) {
+    document.onkeyup = function (event) {
         var keyPressed = String.fromCharCode(event.keyCode).toLowerCase();
         console.log(keyPressed);
 
         // User's guess is compared to chosen letter.
-        if (keyPressed == chosenLet) {
-            wins++;
-            document.getElementById("winsDispl").innerHTML = "Wins: " + wins;
-            winAudio.play();
-            console.log(wins);
-            restart();
-        } else {
-            // Each wrong guess causes "Guesses Left" to decrease by 1.
-            guessesLeft--;
-            document.getElementById("guessesRem").innerHTML = "Guesses Left: " + guessesLeft;
-            console.log(guessesLeft);
-            guessed.push(" " + keyPressed);
-            document.getElementById("guessedLet").innerHTML = "Guessed Letters: " + guessed;
-            console.log(guessed);
-        }
+        if (event.keyCode >= 65 && event.keyCode <= 90) {
 
-        if (guessesLeft == 0) {
-            losses++;
-            document.getElementById("lossesDispl").innerHTML = "Losses: " + losses;
-            loseAudio.play();
-            console.log(losses);
-            restart();
+            var restart = function () {
+                // Not working: getting the computer to choose a new random letter.
+                chosenLet = alphabet[Math.floor(Math.random() * alphabet.length)];
+                console.log(chosenLet);
+                guessesLeft = 7;
+                document.getElementById("guessesRem").innerHTML = "Guesses Left: " + guessesLeft;
+                // Need to not allow the correct letter from previous game to occupy `guessed` array.
+                guessed = [];
+                document.getElementById("guessedLet").innerHTML = "Guessed Letters: ";
+            };
+
+            if (keyPressed == chosenLet) {
+                wins++;
+                document.getElementById("winsDispl").innerHTML = "Wins: " + wins;
+                winAudio.play();
+                console.log(wins);
+                restart();
+            } else {
+                // Each wrong guess causes "Guesses Left" to decrease by 1.
+                guessesLeft--;
+                document.getElementById("guessesRem").innerHTML = "Guesses Left: " + guessesLeft;
+                console.log(guessesLeft);
+                guessed.push(" " + keyPressed);
+                for (var i = 0; i < guessed.length; i++) {
+                    if (keyPressed != guessed.indexOf(i)) {
+                        document.getElementById("guessedLet").innerHTML = "Guessed Letters: " + guessed;
+                        console.log(guessed);
+                    }
+                }
+            };
+
+            if (guessesLeft == 0) {
+                losses++;
+                document.getElementById("lossesDispl").innerHTML = "Losses: " + losses;
+                loseAudio.play();
+                console.log(losses);
+                restart();
+            };
         };
-
-    };
-
-    var restart = function () {
-        // Not working: getting the computer to choose a new random letter.
-        chosenLet = alphabet[Math.floor(Math.random() * alphabet.length)];
-        console.log(chosenLet);
-        guessesLeft = 7;
-        document.getElementById("guessesRem").innerHTML = "Guesses Left: " + guessesLeft;
-        // Need to not allow the correct letter from previous game to occupy `guessed` array.
-        guessed = [];
-        document.getElementById("guessedLet").innerHTML = "Guessed Letters: ";
     };
 };
-
-// Need to not allow letter repeats.
-// Need to not allow symbols other than letters.
+    // Need to not allow letter repeats.
+    // Need to not allow symbols other than letters. (Completed)
